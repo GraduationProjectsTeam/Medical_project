f1e75ca70c64fdbd23d71c00dad548371e6002e2
Tigg <ignatovichanastasiia@gmail.com>
2021-12-24 17:29:16 +0300

    докинула ВСЕ файлы(и технические тоже)

---
+++ src/test/java/ru/geekbrains/com/MedicalProject/MedicalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.com.MedicalProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
